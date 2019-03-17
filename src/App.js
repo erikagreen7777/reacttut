7b22499e728c49fc08b1464cd3e1170ee8ac4e32
Erika Green <erika@Erikas-MacBook-Pro.local>
2019-03-17 14:18:33 -0400

    updating tutorial code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+    { name: 'Max', age: 28},
+    { name: 'Erika', age: 32}
+    ]
+  }
+
+  switchNameHandler = () => {
+    // alert('Was clicked!');
+    // this.state.persons[0].name = 'Maximilian';
+    this.setState({
+      persons: [
+        { name: 'Maximilian', age: 28},
+        { name: 'Erika', age: 30}
+        ] });
+  }
+
   render() {
     return (
       <div className="App">
-        <Person />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>I like cheese</Person>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
-const person = () => {
+const person = (props) => {
 	return (
-		<p> I'm a person!</p>
+		<div>		
+			<p> I'm {props.name} and I'm {props.age} years old!</p>
+			<p>{props.children}</p>
+		</div>
 	);
 }
 
